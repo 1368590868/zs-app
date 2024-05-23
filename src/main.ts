37ed90dffa6dad94fda498077b185a36d2437ce7
mumu <mumu@irlin.cn>
2024-05-24 00:23:50 +0800

    feat add vant

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { createSSRApp } from "vue";
-import { VueQueryPlugin, vueQueryPluginOptions } from 'plugins/queryPlugins'
+import { VueQueryPlugin, vueQueryPluginOptions } from "plugins/queryPlugins";
 import App from "./App.vue";
 export function createApp() {
   const app = createSSRApp(App);
-  app.use(VueQueryPlugin, vueQueryPluginOptions)
+  app.use(VueQueryPlugin, vueQueryPluginOptions);
   return {
     app,
   };
--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,35 @@
 import { defineConfig } from "vite";
 import uni from "@dcloudio/vite-plugin-uni";
 
-
-import { resolve, join } from 'path';
+import { resolve, join } from "path";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { VantResolver, VantImports } from "@vant/auto-import-resolver";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [uni()],
-	resolve: {
-		alias: {
-			components: join(__dirname, 'src/components/'),
-			utils: join(__dirname, 'src/utils/'),
-			store: join(__dirname, 'src/store/'),
-			api: join(__dirname, 'src/api/'),
-			styles: join(__dirname, 'src/styles/'),
-			constants: join(__dirname, 'src/constants/'),
-			hooks: join(__dirname, 'src/hooks/'),
-			static: join(__dirname, 'src/static/'),
-			types: join(__dirname, 'src/types/'),
-			plugins: join(__dirname, 'src/plugins/')
-		}
-	}
-})
+  plugins: [
+    uni(),
+    AutoImport({
+      imports: [VantImports],
+      resolvers: [VantResolver()],
+    }),
+    Components({
+      resolvers: [VantResolver()],
+    }),
+  ],
+  resolve: {
+    alias: {
+      components: join(__dirname, "src/components/"),
+      utils: join(__dirname, "src/utils/"),
+      store: join(__dirname, "src/store/"),
+      api: join(__dirname, "src/api/"),
+      styles: join(__dirname, "src/styles/"),
+      constants: join(__dirname, "src/constants/"),
+      hooks: join(__dirname, "src/hooks/"),
+      static: join(__dirname, "src/static/"),
+      types: join(__dirname, "src/types/"),
+      plugins: join(__dirname, "src/plugins/"),
+    },
+  },
+});
